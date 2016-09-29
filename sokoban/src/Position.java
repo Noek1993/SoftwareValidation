72a0a51b2a6ca2aaada294fb3cb26f6526a00000
Ruben <Athrem@github.com>
2016-09-29 15:00:02 +0200

    position.java gespecificeerd

--- sokoban/src/Position.java
+++ sokoban/src/Position.java
@@ -6,8 +6,8 @@ final class Position
 
   /** @informal only allow positive positions */
   //@ invariant x >= 0 && y >= 0;
-  final int x;
-  final int y;
+  /*@ spec_public */final int x;
+  /*@ spec_public */final int y;
 
   /** @informal based on valid parameters the constructor creates a valid position object */
   //@ requires x >= 0 && y >= 0;
@@ -19,6 +19,10 @@ final class Position
 
   /** @informal to be equal positions need to agree on both coordinates */ 
   //@ requires o instanceof Position;
+  //@ ensures \result == (((Position) o).x == x && ((Position) o).y == y);
+  //@ also
+  //@ requires !(o instanceof Position);
+  //@ ensures \result == false;
   public boolean equals (Object o) {
     if (o instanceof Position) {
       Position q = (Position) o;
@@ -27,14 +31,19 @@ final class Position
     return false;
   }
 
-  /** @informal a valid next position is away one move horizontally
+  /** @informal a valid next position is always one move horizontally
    *    or vertically from the current one */
+  //@ requires Math.abs(newPosition.x - x) + Math.abs(newPosition.y - y) == 1;
+  //@ ensures \result == true;
+  //@ also
+  //@ requires Math.abs(newPosition.x - x) + Math.abs(newPosition.y - y) != 1;
+  //@ ensures \result == false;
   boolean isValidNextPosition (Position newPosition) {
 	  int dX = newPosition.x - x;
 	  int dY = newPosition.y - y;
 	  if( dX >= -1 && dX <= 1 && dY >= -1 && dY <= 1) return true;
 	  return false;
-	  }
+  }
 
   //@ skipesc
   public /*@ pure non_null @*/ String toString () {
