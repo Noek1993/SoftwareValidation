de5969464b6bb66eb22341aefcb15879e6020181
Koen Mulder <Noek1993@outlook.com>
2016-10-13 14:00:53 +0200

    Array bug fix

--- Array Requirements/src/Array.java
+++ Array Requirements/src/Array.java
@@ -24,16 +24,17 @@ public class Array {
 	// index i1 and i2 are  segrement with lenght k, after this we check for the palindrome, we consider the segment i1,i2 a sub array
 	//@ public invariant (\exists int i1, i2; i1 >= 0 && i2 == (i1 + k -1) && i2 < n; (\forall int i3; i3 >= 0 && i3 < k; B[i1 + i3] == B[i2 - i3]));
 	
-	// 1flsls
-	
+	// 1f
 	/*@ public invariant (\exists int i1, i2; i1 >= 0 && i2 > i1 && i2 < n; (\forall int i3; i3 >= 1 && i3 < n; 
 		B[i3-1] <= B[i3] || (i3 == i1 && B[i3-1] <= B[i2]) || (i3-1 == i2 && B[i1] <= B[i3]) || (i3-1 == i1 && i3 == i2 && B[i2] <= B[i1]) ) );
 	 @*/
 	
 	// 1g
 	/*@ public invariant (\exists int i1; i1 >= 0 && i1 < n; (\forall int i2; i2 >= 0 && i2 < n && B[i1] != B[i2]; 
-	  	(\sum int s1; s1 >= 0 && s1 <= n && B[s1] == B[i1]; 1) > (\sum int s2; s2 >= 0 && s2 <= n && B[s2] == B[i2]; 1) ) );
+	  	(\sum int s1; s1 >= 0 && s1 < n && B[s1] == B[i1]; 1) > (\sum int s2; s2 >= 0 && s2 < n && B[s2] == B[i2]; 1) ) );
 	 @*/
+	
+	
 }
 
 
