0e35c6d22fba205f1a6dcf4bff1944b5f82c8f78
Koen Mulder <Noek1993@outlook.com>
2016-11-03 10:07:20 +0100

    Removed unneeded asserts and assumes

--- satabs/search.c
+++ satabs/search.c
@@ -5,20 +5,19 @@ int nondet_int();
 int array[N];
 
 int search(int v, int n) {
-   __CPROVER_assume(n >= 0 && n <= N);
+   assert(n == N);
    int first = 0;
    int last = n - 1;
    int middle = (first+last)/2;
-   assert (first == 0 && last == n-1);
    while (first <= last) {
       __CPROVER_assume(first >=0);
       __CPROVER_assume(last < N);
       if (array[middle] < v) {
-         first = middle+1;
+         first = middle+2;
 		} else if (array[middle] == v) {
          return 1;
       } else {
-         last = middle-1;
+         last = middle-2;
       }
       middle = (first+last)/2;
       assert(middle >= 0 && middle < N);
