10427a013d03ff7a5d921f6b8dd93c76329955c6
Koen Mulder <Noek1993@outlook.com>
2016-11-03 09:54:51 +0100

    Working SATABS expressions

--- satabs/search.c
+++ satabs/search.c
@@ -5,24 +5,23 @@ int nondet_int();
 int array[N];
 
 int search(int v, int n) {
-   __CPROVER_assume(n >= 0 && n <= 64);
+   __CPROVER_assume(n >= 0 && n <= N);
    int first = 0;
    int last = n - 1;
    int middle = (first+last)/2;
-   __CPROVER_assume(first >= 0 && last < n);
-   assert (first == 0 &&last == n-1);
+   assert (first == 0 && last == n-1);
    while (first <= last) {
+      __CPROVER_assume(first >=0);
+      __CPROVER_assume(last < N);
       if (array[middle] < v) {
          first = middle+1;
-         assert (first >= 0 && first < n);
 		} else if (array[middle] == v) {
          return 1;
       } else {
          last = middle-1;
-         assert (last >= 0 && last < n);
       }
       middle = (first+last)/2;
-      assert (middle >= 0 && middle < n);
+      assert(middle >= 0 && middle < N);
    }
    return 0; 
 }
